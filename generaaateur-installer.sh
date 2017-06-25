cf5a1834b18a2f5b06b36377987c2ddc9ae40983
Parmifer <yannbutscher@gmail.com>
2017-06-25 20:21:47 +0200

    Add files via upload

---
+++ generaaateur-installer.sh
@@ -0,0 +1,5 @@
+sudo mv ../vendor/sensio/generator-bundle/Resources/skeleton/crud ../vendor/sensio/generator-bundle/Resources/skeleton/crud.save
+
+mv ../vendor/sensio/generator-bundle/Generator/DoctrineCrudGenerator.php ../vendor/sensio/generator-bundle/Generator/DoctrineCrudGenerator.php.save
+
+cp -R vendor ../
